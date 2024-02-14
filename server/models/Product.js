b9dd078d39303422e7a9f947049a4f831b793d81
zeynep inan <zeyzey6102@gmail.com>
2024-02-14 14:02:40 +0300

    backend eklendi

---
+++ server/models/Product.js
@@ -0,0 +1,19 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+
+const productSchema = new Schema({
+    name: {
+        type:String,
+        required: true
+    },
+    price:{
+        type:Number
+    },
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: 'Category'
+    }
+  });
+
+
+module.exports = mongoose.model('Product', productSchema);
