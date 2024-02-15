0419148e052f6e7c4f65d9b1bdcad69fad00d25d
Pinar <karismazp@gmail.com>
2024-02-15 17:57:28 +0300

    post-delete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,20 @@
 
 import './App.css';
 import React, {useEffect, useState} from "react";
-
-
+const initialState={
+  name:"",
+  category:"",
+};
 function App() {
  const [data1, setData]=useState([]);
+ const [inputValue, setInputValue] = useState(initialState);
+ const {name,category}=inputValue;
 
   useEffect(() => {
     getProducts();
 }, []);
+
+
 
 const getProducts = async () => {
     try {
@@ -28,48 +34,101 @@ const getProducts = async () => {
     }
 };
 
+const handleInputChange = (event) => {
+  setInputValue(event.target.value);
+  console.log(inputValue);
+};
+const handleSubmit=(e)=>{
+  e.preventDefault();
+  if(!name||!category){
+      window.confirm("Please full");
+      return;
+  }
+  ;
+};
+const addProduct = async (name , category) => {
+   
+  await window.fetch('http://localhost:5000/product', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({name , category})
+  }).then(response => {
+    console.log(response)
+  });
+  
+  // Yeni kategori nesnesi oluşturma
+  const newProduct = { name , category}; // Yeni kategori nesnesi
 
-/*const product = data1.map((item, index) => (
-  <tr key={index}>
-      <td>{item._id}</td>
-      <td>{item.name}</td>
-      <td>{item.category}</td>
-      <td><div className="buttons">
-               <button className="btn btn-primary">EDİT</button>
-               <button className="btn btn-priimary">DELETE</button>
-             </div></td>
-  </tr>
-));*/
+  // Mevcut kategori listesine yeni kategoriyi ekleyerek güncelleme yapma
+  const updateProduct = [{...newProduct, name,category}];
+  const response = await window.fetch("http://localhost:5000/product");
+  const data = await response.json(); // Yanıtı JSON olarak çözümleyin
+  const array=Object.values(data.product);
+  setData(array);
+  setInputValue(updateProduct);
+  console.log(updateProduct);
+};  
+const deleteProduct = async (name) => {
+   
+  await window.fetch('http://localhost:5000/product', {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ name })
+  }).then(response => {
+    console.log(response)
+  });
+
+  const updated = data1.filter(item => item.name !== name);
+  setData(updated);
+};  
+
   return (
 
     <div className="App">
       <table>
         <tr>
-            <td>id</td>
             <td>Name</td>
             <td>Category</td>
         </tr>
+        
         <tbody>
          {data1 && data1.map((item,index) => ( 
          <tr key={index}>
-           <td>{item._id}</td>
            <td>{item.name}</td>
            <td>{item.category}</td>
            <td>
              <div className="buttons">
                <button className="btn btn-primary">EDİT</button>
-               <button className="btn btn-priimary">DELETE</button>
+               <button className="btn btn-primary"onClick={() => deleteProduct(item.name)}>DELETE</button>
              </div>
              
            </td>
          </tr>
+          
        ))} 
-      <div className="buttons">
-               <button className="btn btn-primary">YENİ EKLE</button>
-            
-             </div>
+       <tr>
+        <div>
+       <input
+        type="text"
+        value={name}
+        onChange={handleInputChange}
+        placeholder="Bir öğe girin..."
+      />
+      <input
+        type="text"
+        value={category}
+        onChange={handleInputChange}
+        placeholder="Bir öğe girin..."
+      />
+      <button className="addButton" onSubmit={handleSubmit} onClick={() => addProduct(inputValue)}>Ekle</button>
+    </div>
+       </tr>
       
-       </tbody>
+      </tbody>
     </table>
     </div>
   );
